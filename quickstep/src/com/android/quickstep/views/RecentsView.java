78251b56b0f911ece49ae2392217dab2b68ea9c1
Vishalcj17 <vishalcj@aospa.co>
2024-04-12 14:23:49 +0900

    [SQUASH] Revert "Launcher3: disable live tile"
    Fixes Instagram getting stuck and media playback to stop while audio is working.
    
    This reverts commit 8039e8b91efce4e6146fc9e08eff3f270c2fd991.
    
    Change-Id: Iaba540911d1a21392d2f19651a19f9b246b3d606

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -2656,10 +2656,6 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
         animateActionsViewIn();
 
         mCurrentGestureEndTarget = null;
-
-        switchToScreenshot(
-            () -> finishRecentsAnimation(true /* toRecents */, false /* shouldPip */,
-                    null));
     }
 
     /**
